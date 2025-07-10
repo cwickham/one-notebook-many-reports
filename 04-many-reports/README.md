836e6564bda18b2cab7dfd96b8181c399fa9234b
Charlotte Wickham <cwickham@gmail.com>
2025-07-10 15:54:22 -0700

    Clean up

--- 04-many-reports/README.md
+++ 04-many-reports/README.md
@@ -1,11 +1,10 @@
 Starting from `03-one-parameterized-report`
 
-Rename `corvallis.qmd` to `climate.qmd`
-
-Add city list along with filenames `data/cities.csv`
-
+To generate the all 50 reports:
 
 ```{.bash}
 chmod +x gen-reports.py
 ./gen-reports.py
 ```   
+
+Reports will be produced in `reports/`
--- 04-many-reports/gen-reports.py
+++ 04-many-reports/gen-reports.py
@@ -6,14 +6,12 @@ import polars as pl
 cities = (
     pl.read_csv("data/cities.csv")
     .with_columns(
-        [
             pl.col("city")
             .str.strip_chars()
             .str.replace_all(" ", "_")
             .str.replace_all("\\.", "")
             .str.to_lowercase()
             .alias("filename")
-        ]
     )
     .with_columns(
         (pl.col("filename") + pl.lit(".pdf")).alias(
@@ -22,11 +20,9 @@ cities = (
     )
 )
 
-for city, output_file in zip(
-    cities["city"], cities["output_file"]
-):
+for row in cities.iter_rows(named=True):
     render(
-        "climate.qmd",
-        execute_params={"city": city},
-        output_file=output_file,
+        "climate.ipynb",
+        execute_params={"city": row["city"]},
+        output_file=row["output_file"],
     )
